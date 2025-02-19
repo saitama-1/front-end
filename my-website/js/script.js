08cd66ce1053087c90ffee4b5dfe9dec4fa18ca9
saitama-1 <abc@abc.com>
2025-02-19 23:31:42 +0700

    delete

--- my-website/js/script.js
+++
@@ -1,6 +0,0 @@
-// Ví dụ: Thêm sự kiện cho nút "Thêm vào giỏ"
-document.querySelectorAll(".product-card button").forEach((button) => {
-  button.addEventListener("click", function () {
-    alert("Sản phẩm đã được thêm vào giỏ!");
-  });
-});
